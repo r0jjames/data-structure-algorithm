5e88e6bfe5690cb4c0a78206ee6de2e20756fc39
r0jjames <rojjamescarranza@gmail.com>
2024-07-16 14:22:31 +0200

    Add more problems and solutions

--- JAVA_VERSION/DATA-STRUCTURE_ALGORITHM/src/algorithm/common_problems/CommonProblems.java
+++ JAVA_VERSION/DATA-STRUCTURE_ALGORITHM/src/algorithm/common_problems/CommonProblems.java
@@ -8,7 +8,13 @@ import java.util.Set;
 public class CommonProblems {
     public static void main(String[] args) {
 
-
+        int num = 29; // Example number
+        boolean isPrime = isPrime(num);
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
         /*
         // Problem: Find the Longest Common Prefix
         String[] strs = {"flower", "flow", "floght"}; // Example array of strings
@@ -69,7 +75,24 @@ public class CommonProblems {
         System.out.println("Reversed string: " + reversed); // Reversed string: !dlrow ,olleH
          */
     }
-
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        if (num <= 3) {
+            return true; // 2 and 3 are prime
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false; // Numbers divisible by 2 or 3 are not prime
+        }
+        // Check for prime using trial division up to the square root of the number
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false; // If number is divisible by i or i+2, it's not prime
+            }
+        }
+        return true; // If number is not divisible by any smaller prime, it's prime
+    }
     public static int maxSubarraySum(int[] nums) {
         if (nums.length == 0 || nums == null) {
             return 0;
